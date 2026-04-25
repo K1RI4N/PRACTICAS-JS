cfb06e75023b22746d7ff7db008e1dee76c7545e
K1RIAN <mmian.alejandro@gmail.com>
2026-04-25 00:38:48 -0300

    lección de funciones
    incompleto.

---
+++ js/16-funciones.js
@@ -0,0 +1,6 @@
+//Funciones bloque de codigo diseñado para hacer una tarea concreta se pueden reutilizar las veces que queramos
+
+function myFuncion (){ //estructura de una funcion, todo lo que este dentro de la streuct es local a la funcion
+    console.log("hola mi funcion")
+}
+
